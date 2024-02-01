302410863a77909c1d6b8a5cf1117ecd62a44821
trombiano1 <trombiano1@gmail.com>
2024-02-01 21:38:40 +0900

    add: pppso

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,26 @@ extern crate rand;
 
 mod awpso;
 mod defaultpso;
-use std::process::Command;
 mod function;
 mod particle;
+mod pppso;
 mod problems;
 mod pso;
 mod utils;
 use awpso::particle::AWParticle;
 use defaultpso::particle::DefaultParticle;
 use defaultpso::pso::DefaultPSO;
+use pppso::particle::PPParticle;
+use pppso::pso::PPPSO;
 use pso::PSOTrait;
 use std::path::Path;
+use std::process::Command;
 
 fn main() {
   // Experimental Settings
   let problem = problems::f2();
   let dimensions = 30;
-  let particle_count = 100;
+  let particle_count = 1000;
   let iterations = 10000;
 
   // SPSO
@@ -31,6 +34,11 @@ fn main() {
   let mut awpso: DefaultPSO<AWParticle> = DefaultPSO::new(&problem, dimensions, particle_count);
   awpso.run(iterations);
   let _ = awpso.export_global_best_progress(Path::new("data/AWPSO.csv"));
+
+  // PPPSO
+  let mut pppso: PPPSO<PPParticle> = PPPSO::new(&problem, dimensions, particle_count);
+  pppso.run(iterations);
+  let _ = pppso.export_global_best_progress(Path::new("data/PPPSO.csv"));
 
   // Generate progress graph
   let output = Command::new("python").arg("visualizer.py").output().expect("Failed to execute command");
