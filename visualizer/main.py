baa2d49f7781194aacfc92b990be1d1ee8748b57
trombiano1 <trombiano1@gmail.com>
2024-03-23 20:23:41 +0900

    refactor: grid search animation

--- visualizer/main.py
+++ visualizer/main.py
@@ -4,76 +4,76 @@ import numpy as np  # type: ignore
 import pathlib
 import utils
 from pso import PSO
+from tqdm import tqdm  # type: ignore
 import matplotlib.colors as colors  # type: ignore
 
 HOME = pathlib.Path(__file__).parent.parent
 
 
-def plot_grid_search(
-        arg1: str,
-        arg2: str,
-        data_path: pathlib.Path,
-        out_path: pathlib.Path,
-        frame: int = -1
-        ) -> None:
-    print(frame)
-    plt.cla()
-    data = []
-    X = []
-    Y = []
-    if not data_path.exists():
-        raise ValueError(f"Path {data_path} does not exist.")
-    for exp_path in (data_path).glob("*"):
-        global_bests = []
-        for attempt_path in (exp_path).glob("*"):
-            pso = PSO(attempt_path)
-            global_bests.append(pso.global_best_fitness(frame))
-            x = pso.config["method"]["parameters"][arg1]
-            y = pso.config["method"]["parameters"][arg2]
-        X.append(x)
-        Y.append(y)
-        data.append((x, y, np.average(global_bests)))
+class GridSearch:
+    def __init__(self, data_path: pathlib.Path, arg1: str, arg2: str) -> None:
+        self.arg1 = arg1
+        self.arg2 = arg2
 
-    x_range_and_step = utils.get_range_and_step(X)
-    y_range_and_step = utils.get_range_and_step(Y)
+        if not data_path.exists():
+            raise ValueError(f"Path {data_path} does not exist.")
 
-    Z = np.zeros(np.array((x_range_and_step[3], y_range_and_step[3])))
-    for x, y, z in data:
-        ix = int(round((x - x_range_and_step[0]) / x_range_and_step[2], 3))
-        iy = int(round((y - y_range_and_step[0]) / y_range_and_step[2], 3))
-        Z[iy, ix] = z
+        data = []
+        for exp_path in (data_path).glob("*"):
+            for attempt_path in (exp_path).glob("*"):
+                pso = PSO(attempt_path)
+                x = pso.config["method"]["parameters"][arg1]
+                y = pso.config["method"]["parameters"][arg2]
+                data.append((x, y, pso))
 
-    print(np.min(Z), np.max(Z))
-    norm = colors.LogNorm(vmin=10000, vmax=10000000)
+        self.x_r_and_s = utils.get_range_and_step([i[0] for i in data])
+        self.y_r_and_s = utils.get_range_and_step([i[1] for i in data])
 
-    # plt.figure(figsize=(6.0, 6.0))
-    plt.imshow(Z, cmap='viridis', origin='lower',
-               norm=norm,
-               extent=[x_range_and_step[0] - x_range_and_step[2] / 2,
-                       x_range_and_step[1] + x_range_and_step[2] / 2,
-                       y_range_and_step[0] - y_range_and_step[2] / 2,
-                       y_range_and_step[1] + y_range_and_step[2] / 2])
-    plt.xlabel(arg1)
-    plt.ylabel(arg2)
-    plt.title(f"Iteration: {frame}")
-    # plt.savefig(out_path)
+        self.psos = [[PSO(attempt_path) for _ in range(self.y_r_and_s[3])]
+                     for _ in range(self.x_r_and_s[3])]
+        for x, y, z in data:
+            ix = int(round((x - self.x_r_and_s[0]) / self.x_r_and_s[2], 3))
+            iy = int(round((y - self.y_r_and_s[0]) / self.y_r_and_s[2], 3))
+            self.psos[iy][ix] = z
+
+    def plot(
+            self,
+            frame: int = -1
+            ) -> None:
+        plt.cla()
+
+        image = np.zeros((self.x_r_and_s[3], self.y_r_and_s[3]))
+        for (i, row) in enumerate(self.psos):
+            for (j, z) in enumerate(row):
+                image[i, j] = z.global_best_fitness(frame)
+
+        # print(np.min(image), np.max(image))
+        norm = colors.LogNorm(vmin=10000, vmax=10000000)
+
+        plt.imshow(image, cmap='viridis', origin='lower',
+                   norm=norm,
+                   extent=[self.x_r_and_s[0] - self.x_r_and_s[2] / 2,
+                           self.x_r_and_s[1] + self.x_r_and_s[2] / 2,
+                           self.y_r_and_s[0] - self.y_r_and_s[2] / 2,
+                           self.y_r_and_s[1] + self.y_r_and_s[2] / 2])
+        plt.title(f"Iteration: {frame}")
+        self.progressbar.update(1)
+
+    def animate(self, out_path: pathlib.Path) -> None:
+        skip_frames = 10
+        self.progressbar = tqdm(total=int(
+            (len(self.psos[0][0].global_best_fitness_progress())
+             / skip_frames) + 2))
+
+        self.plot(0)
+        plt.colorbar()
+
+        fig, ax = plt.subplots()
+        frames = range(0, 1000, skip_frames)
+        ani = FuncAnimation(fig, self.plot, frames=frames)
+        ani.save(out_path, fps=10)
 
 
-fig, ax = plt.subplots()
-skip_frames = 100
-frames = range(0, 1000, skip_frames)
-
-plot_grid_search("phi_g", "phi_p",
-                 HOME / "data" / "base_pso_test" / "CEC2017_F3",
-                 HOME / "graphs" / "grid_search.png", 0)
-plt.colorbar()
-
-ani = FuncAnimation(fig, lambda x:  plot_grid_search("phi_g", "phi_p",
-                    HOME / "data" / "base_pso_test" / "CEC2017_F3",
-                    HOME / "graphs" / "grid_search.png", x), frames=frames)
-ani.save(HOME / "graphs" / "grid_search.gif", fps=10)
-
-# plot_grid_search(HOME / "data" / "PSO_grid")
-# pso = PSO(HOME / "data" / "PSO_grid" / "p0_g0")
-# pso.animate(HOME / "graphs" / "test.gif")
-# pso.overview(False)
+grid = GridSearch(HOME / "data" / "base_pso_test" / "CEC2017_F4",
+                  "phi_g", "phi_p")
+grid.animate(HOME / "graphs" / "test.gif")
